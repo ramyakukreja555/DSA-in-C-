efe8f6b32fc117830e3a35beb7b3afbf60d64178
https://github.com/ramyakukreja555 <ramyakukreja555@gmail.com>
2026-06-27 04:47:22 +0530

    Added merge sort and quick sort

---
+++ Sorting/quicksort.cpp
@@ -0,0 +1,55 @@
+// pick pivot then smaller on left and greater on right
+// divide and conquer algo
+#include <iostream>
+#include <vector>
+using namespace std;
+int partition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while (i < j)
+    {
+        while (arr[i] <= pivot && i <= high - 1)
+        {
+            i++;
+        }
+        while (arr[j] > pivot && j >= low + 1)
+        {
+            j--;
+        }
+        if (i < j)
+            swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+void qs(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+vector<int> quicksort(vector<int> arr, int n)
+{
+    qs(arr, 0, n - 1);
+    return arr;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    arr = quicksort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+}
